2371a8be835098c8d0eba647aef71025a1d2e6d1
Beau <beaulm@gmail.com>
2016-10-19 18:09:44 -0500

    Add production build

--- README.md
+++ README.md
@@ -5,5 +5,15 @@ To run locally:
 1. Clone this repository (`git clone git@github.com:beaulm/goldfish.git`)
 2. Change into the project directory (`cd goldfish`)
 3. Install dependencies (`npm install`)
-4. Start a dev server (`npm run dev`)
-5. Enjoy (point your browser to <http://localhost:8080/webpack-dev-server/>)
+
+Then you can either run it locally:
+
+1. Start a dev server (`npm run dev`)
+2. Enjoy (point your browser to <http://localhost:8080/webpack-dev-server/>)
+
+or  
+  
+Build a production version:
+
+1. Bundle everything together (`npm run prod`)
+2. Enjoy (double-click index.html to view in your browser)
---
+++ webpack.production.config.js
@@ -0,0 +1,35 @@
+path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./src/index.jsx'
+	],
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.CommonsChunkPlugin('common.js'),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.AggressiveMergingPlugin()
+	]
+}
